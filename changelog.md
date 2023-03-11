02a93b31e31f78b4cb7e6d70e0aae8cf9a03ee67
muhammad.imran <muhammad.imran@joget.com>
2023-03-11 13:08:36 +0800

    second release - refer to changelog

---
+++ changelog.md
@@ -0,0 +1,16 @@
+# Changelog
+
+## 7.0.1
+* Stores the date in timestamp format in the database
+* Added ummalqura-calendar to handle the hijri date on both save and load of element
+* As the hijri date picker needs a format to work, so when display format is not entered, dd-mm-yy will be used as default format
+
+
+## 7.0.0
+#### Initial / First Release
+
+* The date picker is in Hijri Format
+* Allows to store and load the dates / previous values
+* Allow to customize the display date formats e.g. yy-mm-dd, dd-mm-yy etc
+
+
--- src/main/java/org/joget/marketplace/hijripicker/HijriDatePicker.java
+++ src/main/java/org/joget/marketplace/hijripicker/HijriDatePicker.java
@@ -1,43 +1,137 @@
 package org.joget.marketplace.hijripicker;
 
+import com.github.msarhan.ummalqura.calendar.UmmalquraCalendar;
+import java.time.LocalDate;
+import java.time.chrono.HijrahChronology;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAccessor;
+import java.util.Calendar;
 import java.util.Map;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeFieldType;
 import org.joget.apps.app.service.AppUtil;
 import org.joget.apps.form.model.Element;
 import org.joget.apps.form.model.FormBuilderPalette;
 import org.joget.apps.form.model.FormBuilderPaletteElement;
 import org.joget.apps.form.model.FormData;
+import org.joget.apps.form.model.FormRow;
 import org.joget.apps.form.model.FormRowSet;
 import org.joget.apps.form.service.FormUtil;
 
 public class HijriDatePicker extends Element implements FormBuilderPaletteElement {
 
+    private static final String DEFAULT_DISPLAY_FORMAT = "dd-mm-yy";
+
     @Override
     public String renderTemplate(FormData formData, Map dataModel) {
         String template = "hijriDatePicker.ftl";
-        String displayFormat = getPropertyString("format");
-        if (displayFormat !=null && !displayFormat.isEmpty()) {
-            displayFormat = getHijriDateFormat(displayFormat);
-        } else {
-            displayFormat = "iDD-iMM-iYYYY";
-        }
         String value = FormUtil.getElementPropertyValue(this, formData);
+        String displayFormat = getPropertyString("format");
+
+        if (value != null && !value.isEmpty()) {
+            if (displayFormat == null || displayFormat.isEmpty()) {
+                displayFormat = DEFAULT_DISPLAY_FORMAT;
+            }
+
+            String javaDateFormat = getJavaDateFormat(displayFormat);
+            DateTime dt = new DateTime(Long.parseLong(value));
+            int calYear = dt.get(DateTimeFieldType.year());
+            int calMonth = dt.get(DateTimeFieldType.monthOfYear());
+            int calDay = dt.get(DateTimeFieldType.dayOfMonth());
+            LocalDate date = LocalDate.of(calYear, calMonth, calDay);
+            final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(javaDateFormat);
+            value = HijrahChronology.INSTANCE.date(date).format(formatter);
+        }
+
+        String hijriDisplayFormat = "iDD-iMM-iYYYY";
+        if (displayFormat != null && !displayFormat.isEmpty()) {
+            hijriDisplayFormat = hijriDisplayFormat(displayFormat);
+        }
+
         dataModel.put("value", value);
-        dataModel.put("displayFormat", displayFormat);
+        dataModel.put("displayFormat", hijriDisplayFormat);
         String html = FormUtil.generateElementHtml(this, formData, template, dataModel);
         return html;
     }
 
-    protected String getHijriDateFormat(String format) {
-        format = format.toUpperCase();
+    @Override
+    public FormRowSet formatData(FormData formData) {
+        FormRowSet rowSet = null;
+        String id = getPropertyString(FormUtil.PROPERTY_ID);
+        if (id != null) {
+            long longTime = 0;
+            String value = FormUtil.getElementPropertyValue(this, formData);
+            //String binderValue = formData.getLoadBinderDataProperty(this, id);
+            if (value != null && !value.isEmpty()) {
+                // convert the data into the timestamp
+                String format = getPropertyString("format");
+                if (format != null && !format.isEmpty()) {
+                    longTime = processDate(value, format);
+                } else {
+                    longTime = processDate(value, DEFAULT_DISPLAY_FORMAT);
+                }
+            }
+            if (value != null && !value.isEmpty()) {
+                FormRow result = new FormRow();
+                result.setProperty(id, String.valueOf(longTime));
+                rowSet = new FormRowSet();
+                rowSet.add(result);
+            }
+        }
+        return rowSet;
+    }
+
+    private long processDate(String value, String format) {
+        String javaDateFormat = getJavaDateFormat(format);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(javaDateFormat);
+        TemporalAccessor ta = formatter.parse(value);
+        int day = ta.get(ChronoField.DAY_OF_MONTH);
+        int month = ta.get(ChronoField.MONTH_OF_YEAR);
+        int year = ta.get(ChronoField.YEAR_OF_ERA);
+        Calendar cal = new UmmalquraCalendar(year, month - 1, day);
+        return cal.getTime().getTime();
+    }
+
+    private String hijriDisplayFormat(String displayFormat) {
+        String hijriDisplayFormat = getJavaDateFormat(displayFormat);
+        hijriDisplayFormat = hijriDisplayFormat.toUpperCase();
+        hijriDisplayFormat = hijriDisplayFormat.replaceAll("DD", "iDD");
+        hijriDisplayFormat = hijriDisplayFormat.replaceAll("MM", "iMM");
+        hijriDisplayFormat = hijriDisplayFormat.replaceAll("YYYY", "iYYYY");
+        return hijriDisplayFormat;
+    }
+
+    protected static String getJavaDateFormat(String format) {
         if (format.contains("DD")) {
-            format = format.replaceAll("DD", "iDD");
+            format = format.replaceAll("DD", "EEEE");
+        } else {
+            format = format.replaceAll("D", "EEE");
         }
+
         if (format.contains("MM")) {
-            format = format.replaceAll("MM", "iMM");
+            format = format.replaceAll("MM", "MMMMM");
+        } else {
+            format = format.replaceAll("M", "MMM");
         }
-        if (format.contains("YY")) {
-            format = format.replaceAll("YY", "iYYYY");
+
+        if (format.contains("mm")) {
+            format = format.replaceAll("mm", "MM");
+        } else {
+            format = format.replaceAll("m", "M");
         }
+
+        if (format.contains("yy")) {
+            format = format.replaceAll("yy", "yyyy");
+        } else {
+            format = format.replaceAll("y", "yy");
+        }
+
+        if (format.contains("tt") || format.contains("TT")) {
+            format = format.replaceAll("tt", "a");
+            format = format.replaceAll("TT", "a");
+        }
+
         return format;
     }
 
@@ -48,7 +142,7 @@ public class HijriDatePicker extends Element implements FormBuilderPaletteElemen
 
     @Override
     public String getVersion() {
-        return "7.0.0";
+        return "7.0.1";
     }
 
     @Override
@@ -90,5 +184,4 @@ public class HijriDatePicker extends Element implements FormBuilderPaletteElemen
     public String getFormBuilderTemplate() {
         return "<label class='label'>Hijri Date Picker</label><input type='text' />";
     }
-
 }
